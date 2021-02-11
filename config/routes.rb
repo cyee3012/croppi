2fc1ab002bf1ed01b5b4dbfedf21a5aa5a8d551a
Chii Law <cyee3012@hotmail.co.uk>
2021-02-12 00:02:18 +0900

    changed routes because nothing needs to be nested, created new and index views for both benchmark_pics and final_pics

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :final_pics do
-    resources :benchmark_pics
-  end
+  resources :benchmark_pics
+  resources :final_pics
 end
