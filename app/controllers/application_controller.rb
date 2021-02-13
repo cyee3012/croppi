484237fe2158dc96527df844f2df9a6e956f307a
Albin Ngawing <fabianalbin7@gmail.com>
2021-02-13 13:59:37 +0900

    implemented pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+     flash[:alert] = "You are not authorized to perform this action."
+     redirect_to(root_path)
+  end
+
+  #def skip_pundit?
+  #  devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  #end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    skip_authorization
   end
 end
