bc0178e1c355d23ca23ec335682ff613901561fe
Albin Ngawing <fabianalbin7@gmail.com>
2021-02-23 20:47:43 +0900

    added location controller with pundit but not finished yet

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,35 @@
+class LocationsController < ApplicationController
+  before_action :set_locations, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @locations = policy_scope(Location)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_locations
+    @Location = Location.find(params[:id])
+    authorize @Location
+  end
+
+  def location_params
+  end
+end
