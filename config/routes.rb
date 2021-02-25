67922bfad6eb1c171b910c0c1fc0b2dcd728a6d1
Chii Law <cyee3012@hotmail.co.uk>
2021-02-25 20:21:31 +0900

    added edit and update to routes of final pic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :benchmark_pics do
-    resources :final_pics, only: [:new, :create]
+    resources :final_pics, only: [:new, :create, :edit, :update]
   end
   resources :final_pics, only: :show
 end
