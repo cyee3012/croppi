928d8b4d5a46f930998bf0d682b3578a8286c150
Mayu Miyoshi <mayu.miyoshi1313@gmail.com>
2021-02-25 22:20:48 +0900

    Implemented map feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 import "bootstrap";
 import { initSelfie } from './components/final_pic';
 import { previewBenchmark } from './components/benchmark_pic';
+import { initMapbox } from '../plugins/init_mapbox';
 
 
 // Internal imports, e.g:
@@ -39,4 +40,9 @@ if (document.querySelector('.benchmark_pic_photo')) {
   if (document.querySelector('.selfie-video')) {
     initSelfie();
   }
+});
+
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
 });
