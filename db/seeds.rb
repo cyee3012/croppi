69e2d076d52132b2809970b09bcaaf078e554cfd
Mayu Miyoshi <mayu.miyoshi1313@gmail.com>
2021-02-27 16:08:31 +0900

    ho

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 FinalPic.destroy_all
-Location.destroy_all
 BenchmarkPic.destroy_all
 User.destroy_all
 Location.destroy_all
