522855326f9afa283c6af556ef6bd949cc1606fc
Chii Law <cyee3012@hotmail.co.uk>
2021-03-23 23:20:08 +0900

    made image more HD but the positioning is off when taking finish pic...

--- app/javascript/packs/components/final_pic.js
+++ app/javascript/packs/components/final_pic.js
@@ -4,7 +4,7 @@ const initSelfie = () => {
   const video = document.querySelector('.selfie-video');
   const startbutton = document.querySelector('.selfie-startbutton');
   const canvas = document.createElement('canvas');
-  let width = 375;    // We will scale the photo width to this
+  let width = 960;    // We will scale the photo width to this
   let height = 812;
   let streaming = false;
 
